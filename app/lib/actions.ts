5743fe165c076504c5f96e55bb126f511b0994fe
ilana <ilanalipkina@gmail.com>
2025-07-15 09:47:56 +0400

    feat: add form validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,35 +4,81 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { ZodError } from "zod/v4";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  customerId: z.string({
+    invalid_type_error: "Please select a customer.",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+  formValues?: { 
+    customerId?: string;
+    amount?: string;
+    status?: string;
+  }
+};
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get("customerId"),
-    amount: formData.get("amount"),
-    status: formData.get("status"),
-  });
-  const amountInCents = amount * 100;
+export async function createInvoice(prevState: State, formData: FormData) {
+  const customerId = formData.get("customerId")?.toString() || "";
+  const amountStr = formData.get("amount")?.toString() || "";
+  const status = formData.get("status")?.toString() || "";
+
+  const errors: State["errors"] = {};
+
+  if (!customerId) {
+    errors.customerId = ["Please select a customer"];
+  }
+
+  const amount = parseFloat(amountStr);
+  if (!amountStr || isNaN(amount) || amount <= 0) {
+    errors.amount = ["Please enter an amount greater than $0."];
+  }
+
+  if (!["pending", "paid"].includes(status)) {
+    errors.status = ["Please select a valid invoice status."];
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+      message: "Missing Fields. Failed to Create Invoice.",
+      formValues: {customerId, amount: amountStr, status}
+    };
+  }
+
+  const amountInCents = Math.round(amount * 100);
   const date = new Date().toISOString().split("T")[0];
 
   try {
     await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-  } catch {}
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
+
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
